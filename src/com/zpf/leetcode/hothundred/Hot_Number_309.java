45b55ecfff386e9e3eaf48c148e0555897e49427
LittleFlyers <2538285284@qq.com>
2021-10-11 23:44:12 +0800

    热题100-309

--- src/com/zpf/leetcode/hothundred/Hot_Number_309.java
+++ src/com/zpf/leetcode/hothundred/Hot_Number_309.java
@@ -1,26 +1,35 @@
 package com.zpf.leetcode.hothundred;
 
+import com.zpf.leetcode.util.Execute;
 import com.zpf.leetcode.util.Log;
 
-public class Hot_Number_309 {
-    public void get() {
+public class Hot_Number_309 implements Execute {
+    @Override
+    public void execute() {
         int[] a = {1, 2, 3, 0, 2};
         Log.i("结果", maxProfit(a));
     }
 
     public int maxProfit(int[] prices) {
-        int[] get = new int[prices.length];
-        get[0] = 0;
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                get[i] = get[i - 1] + prices[i] - prices[i - 1];
-            }
+        if (prices.length==0){
+            return 0;
         }
-        Log.i("缓存", get);
-        int max = 0;
-        for (int i = 1; i < get.length; i++) {
+        int[][] cache = new int[3][prices.length];
+        //持有
+        cache[0][0] = -prices[0];
+/*        //不持有
+        cache[1][0] = 0;
+        //不持有冷冻
+        cache[2][0] = 0;*/
 
+        for (int i = 1; i < prices.length; i++) {
+            cache[0][i] = Math.max(cache[0][i - 1], cache[1][i - 1] - prices[i]);
+            cache[1][i] = Math.max(cache[2][i - 1], cache[1][i - 1]);
+            cache[2][i] = cache[0][i - 1] + prices[i];
         }
-        return 0;
+        for (int i = 0; i < 3; i++) {
+            Log.i("中间" + i, cache[i]);
+        }
+        return Math.max(cache[1][prices.length-1],cache[2][prices.length-1]);
     }
 }
